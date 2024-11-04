81ac64f053b4a243c2d00fe54889c15cb2311623
szymon <szatamon@gmail.com>
2024-11-04 12:10:00 +0100

    code clan

--- public/frontend/app/controllers/vehicleListController.js
+++ public/frontend/app/controllers/vehicleListController.js
@@ -4,26 +4,26 @@ angular.module('CarRentalApp').controller('VehicleListController', ['$scope', 'V
   $scope.loadError = false;
   $scope.loading = false;
   
-    const loadVehicles = () => {
-      $scope.loading = true;
-      $scope.loadError = false;
+  const loadVehicles = () => {
+    $scope.loading = true;
+    $scope.loadError = false;
 
-      VehicleService.getVehicles().then(response => {
-        $scope.vehicles = response.data;
-        $scope.loading = false;
-      }).catch(error => {
-        console.error('Error loading vehicles:', error);
-        $scope.loadError = true;
-        $scope.loading = false;
-      });
-    };
+    VehicleService.getVehicles().then(response => {
+      $scope.vehicles = response.data;
+      $scope.loading = false;
+    }).catch(error => {
+      console.error('Error loading vehicles:', error);
+      $scope.loadError = true;
+      $scope.loading = false;
+    });
+  };
   
-    $scope.deleteVehicle = (vehicle) => {
-      if (confirm('Are you sure you want to delete this vehicle?')) {
-        VehicleService.deleteVehicle(vehicle.id).then(() => loadVehicles());
-      }
-    };
+  $scope.deleteVehicle = (vehicle) => {
+    if (confirm('Are you sure you want to delete this vehicle?')) {
+      VehicleService.deleteVehicle(vehicle.id).then(() => loadVehicles());
+    }
+  };
   
-    loadVehicles();
-  }]);
+  loadVehicles();
+}]);
 
